a2d38c621f1cd3d96abc11b72acf42903571987f
avi ganach <ag0504122684@gmail.com>
2026-07-14 13:11:14 +0300

    finished level 3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from space_network_lib import SpaceNetwork, Packet, TemporalInterferenceError, DataCorruptedError
+from space_network_lib import SpaceNetwork, Packet, TemporalInterferenceError, DataCorruptedError, LinkTerminatedError, \
+    OutOfRangeError, BrokenConnectionError
 from satellite import Satellite
 import time
 
 def transmission_attempt(packet: Packet):
-    nt = SpaceNetwork(2)
+    nt = SpaceNetwork(3)
     while True:
         try:
             nt.send(p)
@@ -14,11 +15,19 @@ def transmission_attempt(packet: Packet):
             time.sleep(2)
         except DataCorruptedError:
             print("Data retrying ,corrupted...")
+        except LinkTerminatedError:
+            raise BrokenConnectionError("Link lost")
+        except OutOfRangeError:
+            raise BrokenConnectionError("Target out of range")
+
 
 
 s1 = Satellite('Sat1', 100)
-s2 = Satellite('Sat2', 200)
+s2 = Satellite('Sat2', 5000)
 
 p = Packet("Hello Avi",s1, s2)
 
-transmission_attempt(p)
+try:
+    transmission_attempt(p)
+except BrokenConnectionError:
+    print("Transmission failed")
