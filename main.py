9acbd51bc280bed36968d476d3895dfa6efe382b
avi ganach <ag0504122684@gmail.com>
2026-07-16 22:35:05 +0300

    finished level 5

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from space_network_lib import SpaceNetwork, Packet, RelayPacket, Satellite, BrokenConnectionError
 
 
+earth = Satellite('Earth', 0)
 s1 = Satellite('Sat1', 100)
 s2 = Satellite('Sat2', 200)
-earth = Satellite('Earth', 0)
+s3 = Satellite('Sat3', 300)
+s4 = Satellite('Sat4', 400)
 
-final_p = Packet("from Hello Earth!!",s1, s2)
-sat_to_earth_p = RelayPacket(final_p, earth, s1)
+final_p = Packet("from Hello Earth!!",s3, s4)
+p_sat2_to_sat3 = RelayPacket(final_p, s2, s3)
+p_sat1_to_sat2 = RelayPacket(p_sat2_to_sat3, s1, s2)
+p_earth_to_sat1 = RelayPacket(p_sat1_to_sat2, earth, s1)
 
 try:
-    SpaceNetwork.attempt_transmission(sat_to_earth_p)
+    SpaceNetwork.attempt_transmission(p_earth_to_sat1)
 except BrokenConnectionError:
     print("Transmission failed")
